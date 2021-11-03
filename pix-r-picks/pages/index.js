734d4732b8c24049f54d6d0d3de971f2baad5b86
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-11-02 22:13:04 -0500

    INIT NEXT APP

---
+++ pix-r-picks/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Pix-R-Picks</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <p>Hello World</p>
+      </main>
+
+      <footer className="flex items-center justify-center w-full h-24 border-t">
+      </footer>
+    </div>
+  )
+}
