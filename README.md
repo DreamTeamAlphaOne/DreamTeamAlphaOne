ae38a1dcaa84b528134fa421ef3e1fbf6c97c626
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-11-06 18:30:31 -0500

    working detection, -- still needs In time emotion detection

--- README.md
+++ README.md
@@ -5,3 +5,11 @@
 ### Colabs -- Anthony Williams, Michael (jimmie) Hendricks, Michael Ryan, Kassie B-radshaw
 
 **Pix-R-Picks is an app that utilizes facial recognition to analyze your facial emotion and return a pixar movie that correlates with such emotion. The movie is decided by using Text analysis to analyze the movie script for key words to do with emotion.**
+
+
+### References **
+#### Facial Recognition Accredidation
+1. https://www.analyticsvidhya.com/blog/2021/06/learn-how-to-implement-face-recognition-using-opencv-with-python/
+2. Stack Overflow
+3. OpenCV Documentation
+4. TechVidvan
--- pages/face_detection.py
+++ pages/face_detection.py
@@ -1,37 +1,82 @@
 import cv2
 import os
+import dlib
+import numpy as np
 
-# Initialize the Classifier file
-cascPath = os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
 
-faceCascade = cv2.CascadeClassifier(cascPath)
 
-# Face Rules for webcam
-video_capture = cv2.VideoCapture(0)
 
-while True:
-    ret, frames = video_capture.read()
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
-    # print("working-1")
+def findFace():
+    # detector
+    detector = dlib.get_frontal_face_detector()
+    # Predictor
+    predictor = dlib.shape_predictor("pages/shape_predictor_68_face_landmarks.dat")
+    # Initialize the Classifier file
+    cascPath = os.path.dirname(cv2.__file__)+"/data/haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(cascPath)
+    # Face Rules for webcam
+    video_capture = cv2.VideoCapture(0)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor = 1.1, 
-        minNeighbors = 5, 
-        minSize = (30, 30),
-        flags = cv2.CASCADE_SCALE_IMAGE
-    )
+    while True:
+        
+        ret, frames = video_capture.read()
+        gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+        # print("working-1")
+        faces = faceCascade.detectMultiScale(
+            gray,
+            scaleFactor = 1.1, 
+            minNeighbors = 5, 
+            minSize = (30, 30),
+            flags = cv2.CASCADE_SCALE_IMAGE,
+        )
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        # print('working-2')
-    cv2.imshow('Video', frames)
+        # for (x, y, w, h) in faces:
+        #     cv2.rectangle(frames, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        #     # print('working-2')
+        for face in faces:
+            x1 = face.left()
+            y1 = face.top()
+            x2 = face.right()
+            y2 = face.bottom()
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        # print('working3')
-        break
+            cv2.rectangle(frames, pt1 = (x1, y1), pt2 = (x2, y2), color=(255, 0, 0), thickness=4)
 
-# release captured frames
-video_capture.release()
-cv2.destroyAllWindows()
+            landmarks = predictor(image = gray, box = face)
+            # x = landmarks.part(27).x
+            # y = landmarks.part(27).y
+            for n in range(0, 28):
+                x = landmarks.part(n).x
+                y = landmarks.part(n).y
 
+            cv2.circle(faces, center = (x, y), radius = 3, color = (0, 255, 0), thickness=3)
+        # Wait key to stop video screen from shutting down
+        cv2.imshow('Detecting Emotion', frames)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # print('working3')
+            break
+    # release frames
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+findFace()
+
+# def findEmot():
+#     detector = dlib.get_frontal_face_detector()
+#     predictor = dlib.shape_predictor("shape_predictor_68_face_landmark")
+
+#     # video = findFace()
+
+#     # faces = detector()
+#     for face in findFace():
+#         x1 = face.left()
+#         y1 = face.top()
+#         x2 = face.right()
+#         y2 = face.bottom()
+
+#     landmarks = predictor(findFace(), box=face)
+#     x = landmarks.part(27).x
+#     y = landmarks.part(27).y
+
+#     cv2.circle(img=findFace(), center=(x, y), radius=5, color=(0,255,0), thickness =-1)
+
+# findEmot()
