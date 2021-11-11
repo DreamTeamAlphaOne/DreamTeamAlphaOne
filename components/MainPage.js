4cca498b3376b397d7509ce126a2989a62be34db
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-11-10 22:40:48 -0800

    Come on Netlify

--- components/MainPage.js
+++ components/MainPage.js
@@ -1,33 +1,21 @@
 import EmotionPicker from './emotionpicker'
 import SuggestedMovie from './suggestedmovie'
 
-export default function MainPage(props) {
-
-    let showSuggestionBox = true
-
-    function wasEmotionPicked(event) {
-        event.preventDefault();
-        showSuggestionBox = true
-        console.log("Function was triggered")
-        console.log(showSuggestionBox)
-    }
-    
+export default function MainPage(props) { 
     
     return (
-        <main className="grid w-full grid-cols-2 gap-4 p-5">
+        <main className="grid w-full grid-cols-3 gap-2 p-2">
             <div className="w-full p-5 m-5 bg-green-500 border-2 border-green-700">
                 <h1 className="text-4xl text-center bg-green-300 border-green-500 border-solid border-6">Facial Recognition Feature Coming Soon!</h1>
             </div>
         
             <div className="w-full p-4 m-5">
-                
-                { showSuggestionBox ?
-                <SuggestedMovie resources={props.resources}/>
-                :
                 <EmotionPicker handleEmotionChange={props.handleEmotionChange}
-                                emotionState={props.emotionState}
-                                wasEmotionPicked={wasEmotionPicked}/>
-                }
+                emotionState={props.emotionState}/>
+            </div>
+
+            <div className="">
+                <SuggestedMovie resources={props.resources}/>
             </div>
         
         </main>
