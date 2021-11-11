0d8ae8078249f00b6e5debe57fff914d4e69c127
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-11-11 15:53:48 -0800

    Showing content on movie suggestin

--- components/SuggestedMovie.js
+++ components/SuggestedMovie.js
@@ -1,28 +1,24 @@
 export default function SuggestedMovie(props) {
     
-    // console.log("resources in suggestedmovie", props.resources)
-    
-    // if (props.resources && props.resources.length === 0) {
-    //     return;
-    // } else {
-    //     var movie = props.resources[0]
-    // }
+    const movieData = props.resources[0]
+    console.log("movieData", movieData)
     
     return (
         <div className="m-5 bg-purple-500 border-8 border-purple-700 border-solid">
+            
             <div id="suggestionbox">
                 <div id="title" className="m-3">
-                    <h2 className="text-center border-2 border-black">Movie Title</h2>        
+                    <h2 className="text-center border-2 border-black">{movieData ? movieData.title : "Movie Title"}</h2>        
                 </div>
                 <div id="image"></div>
                 <div id="description" className="m-5">
-                    <p>Movie Description goes here</p>
+                    <p>{movieData ? movieData.description : "Movie Description Here"}</p>
                 </div>
                 
             </div>
-
-            <button onClick={()=> props.showSuggestionOrPicker()}className="border-2 border-black align-self-center">Close Suggestion</button>
-            
+            <div className="flex flex-auto justify-content-center">
+                <button onClick={()=> props.showSuggestionOrPicker()}className="border-2 border-black">Close Suggestion</button>
+            </div>
         </div>
     )
 }
