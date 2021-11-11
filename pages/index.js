b47b40c43d49322286af1853c248a9477f72085c
Anthony Williams <Williamsjanthony15@yahoo.com>
2021-11-11 11:04:16 -0600

    added incoming changes from dev merge

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,8 @@
 import Head from 'next/head'
-<<<<<<< HEAD
-import Header from '../components/header'
-import Footer from '../components/Footer'
-import ShowMovie from '../components/show_movie'
-=======
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 import MainPage from '../components/MainPage'
->>>>>>> 90a440f462d04d41ddfc891fcb3368be57a22135
 import Login from '../components/Login'
 
 import { useAuth } from '../contexts/auth'
